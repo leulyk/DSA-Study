3ad2cd0f790f6b6c0db368786711030c128ba461
Leul Yiheyis <leulykpro@gmail.com>
2026-04-22 15:22:03 -0400

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.rs
@@ -0,0 +1,49 @@
+impl Solution {
+    pub fn sort_array(mut nums: Vec<i32>) -> Vec<i32> {
+        let high = nums.len() - 1;
+        Self::quick_sort(&mut nums, 0, high);
+        nums
+    }
+
+    fn quick_sort(nums: &mut Vec<i32>, low: usize, high: usize) {
+        if low >= high || low < 0 || high >= nums.len() {
+            return;
+        }
+
+        let pivot = Self::partition(nums, low, high);
+
+        Self::quick_sort(nums, low, pivot);
+        Self::quick_sort(nums, pivot + 1, high);
+    }
+
+    fn partition(nums: &mut Vec<i32>, low: usize, high: usize) -> usize {
+        let mut pivot = low;
+
+        let mut i = low - 1;
+        let mut j = high + 1;
+
+        loop {
+            loop {
+                i += 1;
+                if nums[i] >= nums[pivot] {
+                    break;
+                }
+            } 
+
+            loop {
+                j -= 1;
+                if nums[j] <= nums[pivot] {
+                    break;
+                }
+            }
+
+            if i >= j {
+                return j;
+            }
+
+            let temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
