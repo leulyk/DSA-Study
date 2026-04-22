f475c2fb3cfae3f5e3ba25ae8d25b638d0082759
Leul Yiheyis <leulykpro@gmail.com>
2026-04-22 15:08:43 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.rs
@@ -0,0 +1,12 @@
+
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        let mut s_chars: Vec<char> = s.chars().collect(); 
+        let mut t_chars: Vec<char> = t.chars().collect();
+
+        s_chars.sort_unstable();
+        t_chars.sort_unstable();
+
+        t_chars == s_chars
+    }
+}
